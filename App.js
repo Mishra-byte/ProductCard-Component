3ac3a231db99425eea32e6d1da20606eb7b73dda
Ayush Mishra <work.ayushhh@gmail.com>
2025-09-24 16:18:04 +0530

    App.js

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ProductCard from "./ProductCard";
+
+function App() {
+  return (
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <h2>Products List</h2>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <ProductCard name="Wireless Mouse" price="25.99" status="In Stock" />
+        <ProductCard name="Keyboard" price="45.50" status="Out of Stock" />
+        <ProductCard name="Monitor" price="199.99" status="In Stock" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
